c21c3cdfc7ba4ee6024b3167403f9bf716911bd4
nibuiro <immay1999@gmail.com>
2021-07-11 14:55:47 +0900

    for windows users

---
+++ README.md
@@ -0,0 +1,6 @@
+# lightest-python-level-virtualization
+
+## How to use
+1. downlaod n install python without path configuration.
+2. rewrite path in batchfile.
+3. double click on explorer.
---
+++ python3810win32.bat
@@ -0,0 +1,2 @@
+SET PYTHONPATH=C:\bin\python3810win32\
+cmd /k "SET PATH=C:\bin\python3810win32\;C:\bin\python3810win32\Scripts\;C:\bin\python3810win32\Library\;C:\bin\python3810win32\Library\bin\;%PATH%"
