f43485896bd6a93feeaa8cd310c10c5fc80c49ad
nibuiro <immay1999@gmail.com>
2021-07-19 10:09:25 +0900

    clean: drive out boilerplate.

--- python3810win32.bat
+++ python3810win32.bat
@@ -1,2 +1,2 @@
 SET PYTHONPATH=C:\bin\python3810win32\
-cmd /k "SET PATH=C:\bin\python3810win32\;C:\bin\python3810win32\Scripts\;C:\bin\python3810win32\Library\;C:\bin\python3810win32\Library\bin\;%PATH%"
+cmd /k "SET PATH=%PYTHONPATH%;%PYTHONPATH%Scripts\;%PYTHONPATH%Library\;%PYTHONPATH%Library\bin\;%PATH%"
